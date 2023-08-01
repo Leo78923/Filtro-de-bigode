86968be270dc9dfa944ada206b6cf8db429debf8
Leo78923 <117394079+Leo78923@users.noreply.github.com>
2023-08-01 15:16:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-
+noseX = 0;
+noseY = 0;
 
 function preload() 
 {
- 
+ bigode = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup() {
   canvas = createCanvas(300, 300);
   canvas.center();
-
+  
   video = createCapture(VIDEO);
   video.size(300, 300);
   video.hide();
@@ -27,14 +28,15 @@ function gotPoses(results)
   if(results.length > 0)
   {
      console.log(results);
-     noseX = results[0].pose.nose.x
-     noseY = results[0].pose.nose.y
+     noseX = results[0].pose.nose.x - 40;
+     noseY = results[0].pose.nose.y + 5;
   }
 }
 
 function draw()   
 {
 image(video, 0, 0, 300,300);
+image(bigode, noseX, noseY, 80, 35);
 }
 
 function takeSnapshot(){    
